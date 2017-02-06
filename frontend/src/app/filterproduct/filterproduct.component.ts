5be50d96761b831fb84f4a01cdc13c75499e9ea4
Lisa <l.kafidoff@gmx.de>
2017-02-06 15:54:39 +0100

    nochmal das gleiche wie davor

---
+++ frontend/src/app/filterproduct/filterproduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filterproduct',
+  templateUrl: './filterproduct.component.html',
+  styleUrls: ['./filterproduct.component.css']
+})
+export class FilterproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
